8e9b1479a7f44c58aaef24a1547dedfe38cfe08a
llayz46 <layzer@outlook.fr>
2023-12-29 23:04:34 +0100

    Remove hover effect for all device except desktop, adding JS for burger menu

--- javascript/script.js
+++ javascript/script.js
@@ -8,3 +8,18 @@ document.addEventListener('scroll', () => {
       header.classList.remove('scroll')
     }
 })
+
+document.addEventListener('scroll', () => {
+  const header = document.querySelector("#myList");
+
+  if (window.scrollY > 0) {
+    header.classList.add('scroll')
+  }
+  else {
+    header.classList.remove('scroll')
+  }
+})
+
+icons.addEventListener("click", () => {
+  nav.classList.toggle("active")
+})
